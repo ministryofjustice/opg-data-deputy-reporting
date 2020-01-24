a72ba3f80f6a4675a99caafa73e148777f6d5c35
james.warren <james.warren@digital.justice.gov.uk>
2020-01-24 15:02:50 +0000

    SP-191 initial commit all circle and tf jobs

---
+++ lambda/healthcheck.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+
+def lambda_handler(event, context):
+
+    api_host = os.environ['BASE_URL']
+
+    print('API URL: ' + api_host)
+
+    r = requests.get(api_host + '/api/health-check/service-status',
+                     verify=False)
+
+    response = {
+        'statusCode': r.status_code,
+        'headers': {
+            "myheader": "response"
+        },
+        'body': r.text,
+    }
+
+    return response
