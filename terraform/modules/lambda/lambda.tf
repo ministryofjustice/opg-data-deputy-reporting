ca7b14ea746e4dbc9ccc71d88b7592357bab35dd
jamesrwarren <58939809+jamesrwarren@users.noreply.github.com>
2020-02-19 12:05:56 +0000

    In 96 (#11)
    * add lambda layer functionality

--- terraform/modules/lambda/lambda.tf
+++ terraform/modules/lambda/lambda.tf
@@ -14,6 +14,7 @@ resource "aws_lambda_function" "lambda_function" {
   handler          = var.handler
   runtime          = "python3.7"
   depends_on       = [aws_cloudwatch_log_group.healthcheck_log_group]
+  layers           = [aws_lambda_layer_version.lambda_layer.arn]
   vpc_config {
     subnet_ids         = var.aws_subnet_ids
     security_group_ids = [data.aws_security_group.lambda_api_ingress.id]
@@ -28,8 +29,22 @@ resource "aws_lambda_function" "lambda_function" {
   }
 }
 
+resource "aws_lambda_layer_version" "lambda_layer" {
+  filename         = data.archive_file.lambda_layer_archive.output_path
+  source_code_hash = data.archive_file.lambda_layer_archive.output_base64sha256
+  layer_name       = "requirements_layer_${var.target_environment}"
+
+  compatible_runtimes = ["python3.7"]
+}
+
 data "archive_file" "lambda_archive" {
   type        = "zip"
   source_dir  = "../lambda_functions/${var.lambda_function_subdir}"
   output_path = "./lambda_${var.lambda_function_subdir}.zip"
+}
+
+data "archive_file" "lambda_layer_archive" {
+  type        = "zip"
+  source_dir  = "../lambda_layers"
+  output_path = "./lambda_layers_${var.lambda_function_subdir}.zip"
 }
