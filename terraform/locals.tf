dc363c7c3e22a0ecae05daca2e3ea3c19935ff03
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-02-20 08:53:49 +0000

    I'm not sure what I'm doing

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,4 +3,5 @@ locals {
   account           = contains(keys(var.accounts), local.environment) ? var.accounts[local.environment] : var.accounts.development
   branch_build_flag = contains(keys(var.accounts), local.environment) ? false : true
   a_record          = local.branch_build_flag ? "${local.environment}.${data.aws_route53_zone.environment_cert.name}" : data.aws_route53_zone.environment_cert.name
+  logger_level      = "${local.logger_level}"
 }
