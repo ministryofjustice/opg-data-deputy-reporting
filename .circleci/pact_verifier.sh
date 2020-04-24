a6a0d0f78b6ae5bf08d3be23fda62df743800018
jamesrwarren <58939809+jamesrwarren@users.noreply.github.com>
2020-04-24 12:12:43 +0100

    in-143 need API_VERSION var

--- .circleci/pact_verifier.sh
+++ .circleci/pact_verifier.sh
@@ -2,7 +2,7 @@
 set -e
 if [ "${CONSUMER_TRIGGERED}" == "false" ]
 then
-    echo "Validating against consumer tag ${PROVIDER_VERSION}"
+    echo "Validating against consumer tag ${API_VERSION}"
     #  Verify current provider git_commit against latest consumer git_commit tagged with v<x>
     ./pact/bin/pact-provider-verifier --provider-base-url=http://localhost:4343 \
     --custom-provider-header 'Authorization: asdf1234567890' \
@@ -10,11 +10,11 @@ then
     --provider="OPG Data" \
     --broker-username="${PACT_BROKER_HTTP_AUTH_USER}" \
     --broker-password="${PACT_BROKER_HTTP_AUTH_PASS}" -r \
-    --consumer-version-tag="${PROVIDER_VERSION}" \
-    --provider-version-tag="${PROVIDER_VERSION}" \
+    --consumer-version-tag="${API_VERSION}" \
+    --provider-version-tag="${API_VERSION}" \
     --provider-app-version="${GIT_COMMIT_PROVIDER}" || echo "Error validating, didn't validate"
 
-    echo "Validating against consumer tag ${PROVIDER_VERSION}_production"
+    echo "Validating against consumer tag ${API_VERSION}_production"
     # Verify current provider git_commit against latest consumer git_commit tagged with v<x>_production
     ./pact/bin/pact-provider-verifier --provider-base-url=http://localhost:4343 \
     --custom-provider-header 'Authorization: asdf1234567890' \
@@ -22,8 +22,8 @@ then
     --provider="OPG Data" \
     --broker-username="${PACT_BROKER_HTTP_AUTH_USER}" \
     --broker-password="${PACT_BROKER_HTTP_AUTH_PASS}" -r \
-    --consumer-version-tag="${PROVIDER_VERSION}_production" \
-    --provider-version-tag="${PROVIDER_VERSION}" \
+    --consumer-version-tag="${API_VERSION}_production" \
+    --provider-version-tag="${API_VERSION}" \
     --provider-app-version="${GIT_COMMIT_PROVIDER}" || echo "Error validating, didn't validate"
 elif [ "${CONSUMER_TRIGGERED}" == "true" ]
 then
