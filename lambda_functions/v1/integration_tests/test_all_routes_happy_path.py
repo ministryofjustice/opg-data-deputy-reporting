cfc1baef3eea6db47df8500429be1b8b43919b3c
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-06-16 15:34:40 +0100

    Stop having to manually include/exclude tests

--- lambda_functions/v1/integration_tests/test_all_routes_happy_path.py
+++ lambda_functions/v1/integration_tests/test_all_routes_happy_path.py
@@ -20,7 +20,7 @@ from lambda_functions.v1.integration_tests.conftest import (
 )
 
 
-@pytest.mark.skip(reason="don't run in CI")
+@pytest.mark.smoke_test
 @pytest.mark.run(order=1)
 @pytest.mark.parametrize("test_config", configs_to_test)
 @cases_data(module=cases_reports_endpoint)
@@ -59,7 +59,7 @@ def test_post_a_report(case_data: CaseDataGetter, test_config):
         )
 
 
-@pytest.mark.skip(reason="don't run in CI")
+@pytest.mark.smoke_test
 @pytest.mark.run(order=2)
 @pytest.mark.parametrize("test_config", configs_to_test)
 @cases_data(module=cases_supporting_docs_endpoint)
@@ -98,7 +98,7 @@ def test_post_a_supporting_doc(case_data: CaseDataGetter, test_config):
             assert returned_parent_id == expected_response_data["parent_id"]
 
 
-@pytest.mark.skip(reason="don't run in CI")
+@pytest.mark.smoke_test
 @pytest.mark.run(order=3)
 @pytest.mark.parametrize("test_config", configs_to_test)
 @cases_data(module=cases_new_checklist_endpoint)
@@ -134,7 +134,7 @@ def test_post_a_new_checklist(case_data: CaseDataGetter, test_config):
         )
 
 
-@pytest.mark.skip(reason="don't run in CI")
+@pytest.mark.smoke_test
 @pytest.mark.run(order=4)
 @pytest.mark.parametrize("test_config", configs_to_test)
 @cases_data(module=cases_update_checklist_endpoint)
@@ -167,7 +167,7 @@ def test_post_an_updated_checklist(case_data: CaseDataGetter, test_config):
         assert returned_submission_id == expected_response_data["submission_id"]
 
 
-@pytest.mark.skip(reason="don't run in CI")
+@pytest.mark.smoke_test
 @pytest.mark.run(order=5)
 @pytest.mark.parametrize("test_config", configs_to_test)
 def test_confirm_data(test_config):
