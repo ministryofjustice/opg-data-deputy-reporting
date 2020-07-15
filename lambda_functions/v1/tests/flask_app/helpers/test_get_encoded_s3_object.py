2342b9f7e3a9500482f1cce61efb853f8ded6dea
Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>
2020-07-15 15:20:39 +0100

    More scenarios

--- lambda_functions/v1/tests/flask_app/helpers/test_get_encoded_s3_object.py
+++ lambda_functions/v1/tests/flask_app/helpers/test_get_encoded_s3_object.py
@@ -2,6 +2,7 @@ import base64
 import os
 
 import boto3
+import pytest
 from moto import mock_s3
 
 from lambda_functions.v1.functions.flask_app.app.api.helpers import (
@@ -12,15 +13,33 @@ from lambda_functions.v1.functions.flask_app.app.api.helpers import (
 Tests to add:
 
 * get file by key
-try to get file but key doesn't exist
+* try to get file but bucket doesn't exist
+* try to get file but key doesn't exist
 try to get file but encoding breaks
 try to get file but saving locally fails
 try to get file but opening locally fails
 """
 
 
-# @pytest.mark.skip(reason="File system structure different on CI")
-def test_get_encoded_s3_object():
+@pytest.mark.parametrize(
+    "test_bucket, test_key, expected_result",
+    [
+        ("valid_bucket", "test_file_on_aws.txt", "overwritten by test"),
+        pytest.param(
+            "invalid_bucket",
+            "test_file_on_aws.txt",
+            "overwritten by test",
+            marks=pytest.mark.xfail(reason="Not handled"),
+        ),
+        pytest.param(
+            "valid_bucket",
+            "not_a_file_on_aws.txt",
+            "overwritten by test",
+            marks=pytest.mark.xfail(reason="Not handled"),
+        ),
+    ],
+)
+def test_get_encoded_s3_object(test_bucket, test_key, expected_result):
     path_to_current_file = os.path.realpath(__file__)
     current_directory = os.path.split(path_to_current_file)[0]
     test_file = os.path.join(current_directory, "test_file.txt")
@@ -34,7 +53,7 @@ def test_get_encoded_s3_object():
         s3_client.upload_file(test_file, "valid_bucket", "test_file_on_aws.txt")
 
         result = get_encoded_s3_object(
-            s3_client=s3_client, bucket="valid_bucket", key="test_file_on_aws.txt"
+            s3_client=s3_client, bucket=test_bucket, key=test_key
         )
 
         assert result == expected_result
