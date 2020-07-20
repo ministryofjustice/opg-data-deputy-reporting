9c4a0c74cc15868472a08a719c46b0574b7bbeee
jamesrwarren <58939809+jamesrwarren@users.noreply.github.com>
2020-07-20 09:45:07 +0100

    In 262 a - fix pipeline to prod
    Co-authored-by: Jennifer Mackown <jennifer.mackown@digital.justice.gov.uk>

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
-for i in deputy-reporting-openapi*.yml
-do
-    version=$(echo "${i}" | sed 's/deputy-reporting-openapi-//g' | sed 's/.yml//g')
-    cp "${i}" lambda_functions/"${version}"/deputy-reporting-openapi.yml
-done
-
 for d in lambda_functions/v*/
 do
     find "${d}/functions" -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum | cut -c 1-15 > "${d}"/directory_sha
