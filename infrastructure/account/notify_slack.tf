6deebdf4f4df51cc6c879342ec86c9a7989224e3
jamesrwarren <james.warren@digital.justice.gov.uk>
2022-08-11 09:54:09 +0100

    further tweaks after testing

--- infrastructure/account/notify_slack.tf
+++ infrastructure/account/notify_slack.tf
@@ -8,7 +8,7 @@ resource "aws_sns_topic" "deputy_reporting" {
 }
 
 module "notify_slack" {
-  source = "github.com/terraform-aws-modules/terraform-aws-notify-slack.git?ref=v5.1.0"
+  source = "github.com/terraform-aws-modules/terraform-aws-notify-slack.git?ref=v5.3.0"
 
   sns_topic_name   = aws_sns_topic.deputy_reporting.name
   create_sns_topic = false
@@ -18,7 +18,7 @@ module "notify_slack" {
   cloudwatch_log_group_retention_in_days = 7
 
   slack_webhook_url = data.aws_secretsmanager_secret_version.slack_webhook_url.secret_string
-  slack_channel     = terraform.workspace == "production" ? "#jims_private_channel" : "#jims_private_channel"
+  slack_channel     = terraform.workspace == "production" ? "#opg-digideps-devs" : "#opg-digideps-team"
   slack_username    = "aws"
   slack_emoji       = ":warning:"
 }
