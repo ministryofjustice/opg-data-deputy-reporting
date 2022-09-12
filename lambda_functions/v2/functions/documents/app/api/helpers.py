19f2a9b68d4d4c3867be133f0cc5f42dbec9bf75
jamesrwarren <james.warren@digital.justice.gov.uk>
2022-09-12 16:25:52 +0100

    add extra logging

--- lambda_functions/v2/functions/documents/app/api/helpers.py
+++ lambda_functions/v2/functions/documents/app/api/helpers.py
@@ -278,13 +278,11 @@ def get_digideps_s3_client():
 
 
 def get_encoded_s3_object(s3_client, bucket, key):
-
+    print(f"{key}, bucket: {bucket}")
     try:
         s3_client.download_file(bucket, key, "/tmp/{}".format(key))
     except Exception as e:
-        logger.error(
-            f"Error downloading file from S3 - key: {key}, bucket:  {bucket}, error: {e}"
-        )
+        logger.error(f"Error downloading file from S3: {e}")
         return None
 
     try:
