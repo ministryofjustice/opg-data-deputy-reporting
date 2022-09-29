a9840548ffe91bcf550f670c0a09b5e18a56bfc8
Jim <58939809+jamesrwarren@users.noreply.github.com>
2022-09-29 09:48:43 +0100

    DDPB-4084 slack alarms for digideps team (#145)

--- infrastructure/environment/modules/lambda/outputs.tf
+++ infrastructure/environment/modules/lambda/outputs.tf
@@ -2,3 +2,8 @@ output "lambda" {
   description = "The lambda function"
   value       = aws_lambda_function.lambda_function
 }
+
+output "lambda_log" {
+  description = "The lambda logs"
+  value       = aws_cloudwatch_log_group.lambda
+}
