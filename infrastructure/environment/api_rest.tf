27776d099d3f13a7b2285b7c20c36ff3dbceb481
Jim <58939809+jamesrwarren@users.noreply.github.com>
2024-07-18 16:05:34 +0100

    DDLS-278 - Manage permissions from openapi spec during deployments properly (#235)
    * DDLS-278 fix permissions issues with deployment

--- infrastructure/environment/api_rest.tf
+++ infrastructure/environment/api_rest.tf
@@ -12,5 +12,12 @@ resource "aws_api_gateway_rest_api" "deputy_reporting" {
     types = ["REGIONAL"]
   }
 
+  # This is important to manage the update of roles form openapi spec properly
+  lifecycle {
+    replace_triggered_by = [
+      aws_iam_role.data_deputy_reporting
+    ]
+  }
+
   tags = local.default_tags
 }
