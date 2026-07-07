4e2fb3430233a325977a28b9ae8b96105c2c6ee3
jamesrwarren <james.warren@digital.justice.gov.uk>
2026-07-07 17:07:11 +0100

    DDLS-1552 add service area default tag

--- infrastructure/environment/provider.tf
+++ infrastructure/environment/provider.tf
@@ -13,7 +13,9 @@ terraform {
 
 provider "aws" {
   region = "eu-west-1"
-
+  default_tags {
+    tags = local.default_tags
+  }
   assume_role {
     role_arn     = "arn:aws:iam::${local.account.account_id}:role/${var.default_role}"
     session_name = "terraform-session"
@@ -23,7 +25,9 @@ provider "aws" {
 provider "aws" {
   region = "eu-west-1"
   alias  = "management"
-
+  default_tags {
+    tags = local.default_tags
+  }
   assume_role {
     role_arn     = "arn:aws:iam::311462405659:role/${var.management_role}"
     session_name = "terraform-session"
